13d2b4fbed6e6a4e54f4c561558db2367d9d4c92
Scald <104459145+Arikatsu@users.noreply.github.com>
2024-07-22 17:23:13 +0530

    ok

--- src/screens/bannerManagement/BannerManagementList.jsx
+++ src/screens/bannerManagement/BannerManagementList.jsx
@@ -155,7 +155,7 @@ const BannerManagementList = () => {
       <Modal
         show={showLayout}
         onHide={() => setShowLayout(false)}
-        size={currentCollageType === BannerIds?.MOBILE_SLIDER.id || currentCollageType?.includes("'WEB") ? 'lg' : 'sm'}
+        size={currentCollageType === BannerIds?.MOBILE_SLIDER.id || currentCollageType?.includes("WEB") ? 'lg' : 'sm'}
         centered
         scrollable
         className="mobBannerLayoutPreview"
